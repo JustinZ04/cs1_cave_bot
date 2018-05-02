3452e140c180d57ebeaebe61520fd4c7c63a71e3
Justin <jzabel04@knights.ucf.edu>
2018-05-02 00:22:05 -0400

    Added logic for finding the current time by rounding up to the nearest hour. Probably a cleaner way.

--- parse_excel.py
+++ parse_excel.py
@@ -1,12 +1,10 @@
-from typing import List, Union
-
 from openpyxl import load_workbook
 from datetime import date
 import calendar
 
 
 def parse():
-    wb = load_workbook(filename='office_hours.xlsx')
+    wb = load_workbook(filename='office_hours.xlsx', data_only=True)
     ws = wb.active
 
     # Get the current weekday
@@ -17,8 +15,24 @@ def parse():
     # Search through the first few cells to find the correct column
     for cell in ws.iter_cols(min_col=0, max_col=31, min_row=0, max_row=2):
         #  print(cell[1].value)
+        print(cell)
         if cell[1].value == weekday:
             print(cell[1].value)
+
+    #  This wont work
+    if ws['A3'].value == '8:30':
+        print('matched')
+
+    #  I really hope you know a better way to do this or
+    #  no one is ever going to be able to see this code
+    s = str(ws['A3'].value)
+    s = s.replace(":", "")
+    s = s.replace(" - ", "")
+    s = s.replace("A", "")
+    s = s.replace("P", "")
+    s = s.replace("M", "")
+    s = s[3:]
+    print(s)
 
 
 if __name__ == '__main__':
