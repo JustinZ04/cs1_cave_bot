e89428d3c9144aaf2bfe2567f6d706d841e33ed7
Justin <jzabel04@knights.ucf.edu>
2018-05-03 19:44:42 -0400

    It actually works

--- parse_excel.py
+++ parse_excel.py
@@ -1,33 +1,68 @@
 from openpyxl import load_workbook
 from datetime import date
+import time
 import calendar
 
 
 def parse():
+    time_list = []
+    str_time_list = []
     wb = load_workbook(filename='office_hours.xlsx', data_only=True)
     ws = wb.active
 
     # Get the current weekday
     day = date.today()
+    cur_time = time.localtime()
+    cur_time = time.strftime("%H%M", cur_time)
+
+    for cell in ws.iter_cols(min_col=0, max_col=1, min_row=3, max_row=27):
+        continue
+
+    print(cell)
+    for i in range(25):
+        s = str(cell[i].value)
+        # print(s)
+
+        if s != "None":
+            print(s)
+            s = s.split(' - ')
+        #  print(len(s))
+            s[0] = s[0].replace(":", "")
+        #  print(s[1])
+            s[1] = s[1].replace(":", "")
+            s[1] = s[1].replace(" AM", "")
+            s[1] = s[1].replace(" PM", "")
+            time_list.append(s)
+    print(time_list)
+
+    if int(cur_time) > 1200:
+        cur_time = str(int(cur_time) - 1200)
+
+    print(type(cur_time))
     weekday = calendar.day_name[day.weekday()]
     print(weekday)
 
     # Search through the first few cells to find the correct column
     for cell in ws.iter_cols(min_col=0, max_col=31, min_row=0, max_row=2):
         #  print(cell[1].value)
-        print(cell)
+        #  print(cell)
         if cell[1].value == weekday:
             print(cell[1].value)
 
-    #  This wont work
-    if ws['A3'].value == '8:30':
-        print('matched')
-
     #  I think this is a better way
     s = str(ws['A3'].value)
-    s = s[:4]
-    s = s.replace(":", "")
+    s = s.split(' - ')
     print(s)
+
+    s[0] = s[0].replace(":", "")
+    s[1] = s[1].replace(":", "")
+    s[1] = s[1].replace(" AM", "")
+    s[1] = s[1].replace(" PM", "")
+    print(s)
+
+    #  s = s[:4]
+    #  s = s.replace(":", "")
+    #  print(s)
 
     #  I really hope you know a better way to do this or
     #  no one is ever going to be able to see this code
