fa678910b58ef6c000a9fcc4c0981f75c5e88f95
Justin <jzabel04@knights.ucf.edu>
2018-05-08 14:53:49 -0400

    removed extra comments and timing logic

--- discord_bot.py
+++ discord_bot.py
@@ -1,7 +1,5 @@
 import discord
-import asyncio
 import config
-from datetime import datetime
 
 from parse_excel import parse
 
@@ -20,25 +18,13 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('!cave'):
         ta_list = parse()
-        #print(len(ta_list))
+
         if ta_list is None:
             await client.send_message(message.channel, "No TA's are in the cave right now!")
 
         else:
-            # await client.send_message(message.channel, "Command received")
-
             s = '\n\n\n'.join(ta_list)
-
-
             await client.send_message(message.channel, s)
-
-            # if i != len(ta_list)-1:
-            #    await client.send_message(message.channel, '----------')
-
-    cur_time = datetime.now().time()
-    file = open("logs/commands.log", "a")
-    file.write(str(cur_time) + " Cave command typed\n")
-    file.close()
 
 
 client.run(config.bot_token)
--- parse_excel.py
+++ parse_excel.py
@@ -1,6 +1,6 @@
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter, column_index_from_string
-from datetime import date, datetime
+from datetime import date
 import time
 import calendar
 import re
@@ -14,94 +14,64 @@ def parse():
 
     ws = wb['Office Hours']  # Will have to change the name of the worksheet each time
     ta_list = []
-    # print(ws['J22'].value)
 
     # Get the current weekday
     day = date.today()
     weekday = calendar.day_name[day.weekday()]
-    # noon_flag = False
-    # add a return here so we don't keep going
-    # print(weekday)
+
     if weekday == 'Saturday' or weekday == 'Sunday':
-        # print('---No TAs have office hours at this time!')
+        print('No TAs have office hours at this time!')
         return None
 
     cur_time = time.localtime()
     cur_time = time.strftime("%H%M", cur_time)
     cur_time = int(cur_time)
-    # cur_time = 1530
 
     if cur_time < 900 or cur_time > 2100:
         return None
 
-    #  if int(cur_time) >= 1200:
-    #  noon_flag = True
-    # print('Current time is ' + str(cur_time))
     time_range = None
-    #  Will probably have to edit min/max row for each spreadsheet
 
     # Should be 43 but upper bound of range is exclusive
     for j in range(5, 44):
-        # print("here")
         cell = str(get_column_letter(1) + str(j))
-        # print(cell)
+
         if str(ws[cell].value) != "None":
             s = str(ws[cell].value)
-            # print(s)
             s = s.split(' - ')
             s[0] = s[0].replace(":", "")
             s[1] = s[1].replace(":", "")
             s[1] = s[1].replace(" AM", "")
             s[1] = s[1].replace(" PM", "")
 
-            if cur_time > 1259:  # curtime here
-                temp = cur_time - 1200  # curtime
+            if cur_time > 1259:
+                temp = cur_time - 1200
             else:
-                temp = cur_time  # curtime
+                temp = cur_time
             if abs(temp - int(s[0])) < 30:
                 #  print(ws[cell].row)
                 time_range = ws[cell].row
                 break
 
     if time_range is None:
-        # print('+++No TAs have office hours at this time!')
+        print('No TAs have office hours at this time!')
         return None
 
     found_ta = False
-    #  Will probably have to edit this for each spreadsheet
-
-    # print(time_range)
-    seen_ta = {}
-    stime = datetime.now().time()
-    file = open("times.log", "a")
-    file.write(str(stime) + "\n")
-    file.close()
-
-    #  for i in range(column_index_from_string((weekday_dict[weekday])[0]),
-    #                column_index_from_string((weekday_dict[weekday])[1])+1):
-    #    for j in range(time_range, 4, -1):
 
     i = column_index_from_string((weekday_dict[weekday])[0])
     while i <= column_index_from_string((weekday_dict[weekday])[1]):
         j = time_range
-        # print(j)
+
         while j >= 5:  # may need to change the 5 to account for lower bound of summer spreadsheet
             col = get_column_letter(i)
             row = str(j)
             cell = "".join((col, row))
-
-            # print(cell)
             s = ws[cell].value
-            # print(type(s))
-            # print(s)
+
             if s is None:
                 j -= 1
-                # print("none")
                 continue
-            # if type(s) is not str:
-            #    continue
-
-            # print(s)
 
             office_hour = re.findall(r'\d?\d:\d{2} - \d?\d:\d{2}', s)
 
@@ -117,29 +87,20 @@ def parse():
                     if office_hour[1] < 900:  # might need to change for new lower TA hour bound
                         office_hour[1] = office_hour[1] + 1200
 
-                    if office_hour[0] <= cur_time < office_hour[1]:  # curtime here
+                    if office_hour[0] <= cur_time < office_hour[1]:
                         ta_list.append(ws[cell].value)
                         found_ta = True
-                        #  print("found ta")
                         i += 1
                         break
                     else:
                         i += 1
                         break
             j = j - 1
-        #  print("dec j")
         i = i + 1
 
     if not found_ta:
         print("No TAs have office hours at this time!")
         return None
-
-    # print(ta_list)
-
-    etime = datetime.now().time()
-    file = open("times.log", "a")
-    file.write(str(etime) + "\n")
-    file.close()
 
     return ta_list
 
