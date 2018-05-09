921a80a2c301fee6fee5316b0ee17e9bd65e7d5a
Justin <jzabel04@knights.ucf.edu>
2018-05-09 17:47:22 -0400

    Added comments. This is the working demo version.

--- discord_bot.py
+++ discord_bot.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+# Script to run the discord bot itself.
+# Handles logging in and receiving the correct command to run the parse script. Outputs the current TA's holding office
+# hours.
+
 import discord
 import config
-from datetime import datetime
 
+from datetime import datetime
 from parse_excel import parse
 
 client = discord.Client()
@@ -9,6 +14,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+    # Logs the bot in so it goes online.
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
@@ -17,6 +23,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # Receives the '!cave' command and returns the current TA's holding office hours.
     if message.content.startswith('!cave'):
         ta_list = parse()
 
@@ -27,10 +34,11 @@ async def on_message(message):
             s = '\n\n\n'.join(ta_list)
             await client.send_message(message.channel, s)
 
+        # Log every time a '!cave' command is received to a log file.
         cur_time = datetime.now().time()
         file = open("logs/commands.log", "a")
         file.write(str(cur_time) + " Cave command typed\n")
         file.close()
 
-
+# Holds the authorization token for the bot.
 client.run(config.bot_token)
