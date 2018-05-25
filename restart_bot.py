273ac31439da1a9d07e368e4f5a4ba550dd3dfb5
Justin <jzabel04@knights.ucf.edu>
2018-05-24 20:25:21 -0400

    Restart script now texts administrators when the bot goes down and was restarted

--- restart_bot.py
+++ restart_bot.py
@@ -4,12 +4,21 @@ Check to see if an process is running. If not, restart.
 Run this in a cron job
 """
 import os
+import config
+
+from datetime import datetime
 from subprocess import call
+from twilio.rest import Client
 process_name= "discord_bot.py" # change this to the name of your process
 
 tmp = os.popen("ps -Af").read()
 
 if process_name not in tmp[:]:
    
-   call("python3 discord_bot.py", shell = True) 
-
+   call("python3 discord_bot.py", shell = True)
+   cur_time = datetime.now().time()
+   c = Client(config.sms_sid, config.sms_token)
+   c.messages.create(body='The discord bot crashed and was restarted at ' + cur_time, from_=str(+14078900127),
+                          to=config.m_phone)
+   c.messages.create(body='The discord bot crashed and was restarted at ' + cur_time, from_=str(+14078900127),
+                          to=config.j_phone)
